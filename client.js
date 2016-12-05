f4fcfe7c0dae228e5f27fd59bd61a8ac31f78f0e
Karla Hugo <karlabrandon817@gmail.com>
2016-12-05 15:31:12 -0600

    buttons append to dom on click

--- client.js
+++ client.js
@@ -2,10 +2,23 @@
 //time the corresponding color is clicked. Also, as each color (button) is clicked, it should
 //increment the corresponding label. HTML and CSS are provided, the focus here is jQuery.
 
+//var output = '';
+var i = 1;
 
 $(document).ready(function() {
     $(".color-button").on('click', function() {
         var buttonColor = $(this).attr('data-color');
         console.log('buttonColor:', buttonColor);
+        if (buttonColor === "red") {
+            $(".container").append("<div class='color-cube red'></div>");
+        } else if (buttonColor === "yellow") {
+            $(".container").append("<div class='color-cube yellow'></div>");
+        } else if (buttonColor === "green") {
+            $(".container").append("<div class='color-cube green'></div>");
+        } else {
+            $(".container").append("<div class='color-cube blue'></div>");
+        }
     });
-});
+
+
+}); //end doc ready function
