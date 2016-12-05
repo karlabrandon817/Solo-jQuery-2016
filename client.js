40f85f19b9a47f761c0dd14f183d607e6304d01a
Karla Hugo <karlabrandon817@gmail.com>
2016-12-05 16:04:47 -0600

    increment colors

--- client.js
+++ client.js
@@ -2,20 +2,31 @@
 //time the corresponding color is clicked. Also, as each color (button) is clicked, it should
 //increment the corresponding label. HTML and CSS are provided, the focus here is jQuery.
 
-var i = 1;
+red = 0;
+yellow = 0;
+green = 0;
+blue = 0;
 
 $(document).ready(function() {
     $(".color-button").on('click', function() {
         var buttonColor = $(this).attr('data-color');
         console.log('buttonColor:', buttonColor);
         if (buttonColor === "red") {
+            red++;
             $(".container").append("<div class='color-cube red'></div>");
+            $("#red").html("Total red: " + red);
         } else if (buttonColor === "yellow") {
+            yellow++;
             $(".container").append("<div class='color-cube yellow'></div>");
+            $("#yellow").html("Total yellow: " + yellow);
         } else if (buttonColor === "green") {
+            green++;
             $(".container").append("<div class='color-cube green'></div>");
+            $("#green").html("Total green: " + green);
         } else {
+            blue++;
             $(".container").append("<div class='color-cube blue'></div>");
+            $("#blue").html("Total blue: " + blue);
         }
     });
 
